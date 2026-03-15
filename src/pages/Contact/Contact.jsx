a6e9da0f23aae4fa2c5e236c514adebc7a2d04a7
Rishikesh Sahu <rishiuttamsahu@gmail.com>
2026-03-15 16:27:46 +0530

    ff

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,7 +1,36 @@
 import "./Contact.css";
+import { useState } from "react";
 import { FaEnvelope, FaPhone, FaMapMarkerAlt } from "react-icons/fa";
 
 function Contact() {
+
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+    formData.append("access_key", "af72904e-c920-49e6-b977-a1e53b42f9e2");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("✅ Message Sent Successfully!");
+      event.target.reset();
+    } else {
+      setResult("❌ Something went wrong. Try again!");
+    }
+
+    // 3 second baad gayab ho jaayega
+    setTimeout(() => {
+      setResult("");
+    }, 1500);
+  };
 
   return (
     <div className="contact" id="contact">
@@ -13,7 +42,6 @@ function Contact() {
       <div className="contact-container">
 
         {/* LEFT INFO */}
-
         <div className="contact-info">
 
           <h3>Get In Touch</h3>
@@ -35,36 +63,45 @@ function Contact() {
 
           <div className="info-item">
             <FaMapMarkerAlt className="contact-icon" />
-            <span>Bhiwandi, Maharashtra</span>
+            <span>Pune, Maharashtra</span>
           </div>
 
         </div>
 
         {/* RIGHT FORM */}
-
-        <form className="contact-form">
+        <form className="contact-form" onSubmit={onSubmit}>
 
           <input
             type="text"
+            name="name"
             placeholder="Your Name"
             required
           />
 
           <input
             type="email"
+            name="email"
             placeholder="Your Email"
             required
           />
 
           <textarea
+            name="message"
             placeholder="Your Message"
             rows="6"
             required
           ></textarea>
 
-          <button type="submit">
-            Send Message
-          </button>
+          <button type="submit">Send Message</button>
+
+          {result && (
+            <span className={`form-result ${
+              result.includes("Successfully") ? "success" :
+              result.includes("Sending") ? "sending" : "error"
+            }`}>
+              {result}
+            </span>
+          )}
 
         </form>
 
